54c9906a79b3a0afb4f544c869d1bf565bf9a4ce
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-08 22:11:53 +0530

    added database connection and env url

---
+++ config/dbConnection.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const dbConnection = async() =>{
+
+    mongoose
+    .connect(process.env.MONGO_URL)
+    .then((conn) =>{
+        console.log(`Connection is established succesfully to ${conn.connection.host}`);
+    })
+    .catch((err) =>{
+        console.log(err.message);
+        process.exit(1);
+    })
+}
