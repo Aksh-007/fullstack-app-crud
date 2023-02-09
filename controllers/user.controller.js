820208c9dc2f69d7a7fce27c24441c1516bc4d43
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-09 10:17:14 +0530

    added usercontroller function and route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,35 @@
 //here we write logic of app
+import userModel from '../models/user.schema.js'
 
 export const homeController = (req_, res) => {
     res.send("App is running from home controller ");
 }
 
-export const userController = (req_, res) => {
-    res.send("App is running from home controller user routes ");
-}
+export const createUser = async (req, res) => {
+    try {
+        const { name, email } = req.body;
+        //check all the details
+        if (!name || !email) {
+            throw new Error("name and email are required");
+        }
+
+        const userExist = await userModel.findOne({ email });
+        if (userExist) {
+            throw new Error("Email already Exist");
+        }
+
+        const newUser = await userModel.create({ name, email });
+        res.status(201).json({
+            success:true,
+            message:`User Created Succesfully`,
+            newUser
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+};
+
+export const getUser
+
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,9 @@
 import express from 'express'
 const router = express.Router();
-import {homeController, userController}  from '../controllers/user.controller.js';
+import {createUser, homeController}  from '../controllers/user.controller.js';
 
 //this is url part 
 router.get('/',homeController );
-router.get('/user', userController );
+router.post('/createUser', createUser );
 
 export default router;
